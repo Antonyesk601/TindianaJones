c24b4894cc498d5e9233f162201c49300b061460
Kareem Morsy <kreem.morsy@hotmail.com>
2020-04-02 22:11:16 +0200

    Refactoring
    Added Player Movement
    Fixed Tilemaps

--- Assets/GameAssets/Scripts/Movement.cs
+++ Assets/GameAssets/Scripts/Movement.cs
@@ -2,21 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Movement : MonoBehaviour
 {
     public float speed;
 
-    // Update is called once per frame
-    void Update()
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private bool isLeft = false;
+
+    void Start()
     {
-        float moving = Input.GetAxis("Horizontal") * speed;
-        if (moving > speed)
-        {
-            moving = speed;
-            Debug.Log(moving + " " + speed);
-        }
-        gameObject.transform.Translate(moving, 0, 0);
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movingSpeed = Input.GetAxis("Horizontal") * speed;
+
+        rigidBody.velocity = Vector2.right * movingSpeed + Vector2.up * rigidBody.velocity.y;
+
+        animator.SetFloat("speed", Mathf.Abs(rigidBody.velocity.x));
+        if (rigidBody.velocity.x < 0)
+            isLeft = true;
+        else if (rigidBody.velocity.x > 0)
+            isLeft = false;
+
+        spriteRenderer.flipX = isLeft;
+
 
     }
 }
