6c6ce31348cb1e7d61d2495887faffd62e13db10
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2020-04-03 00:45:53 +0200

    Some changes

--- Assets/GameAssets/Scripts/tilemapGenerator.cs
+++ Assets/GameAssets/Scripts/tilemapGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class tilemapGenerator : MonoBehaviour
+public class TilemapGenerator : MonoBehaviour
 {
     public Grid grid;
     public List<Tilemap> Environment= new List<Tilemap>();
@@ -11,21 +11,24 @@ public class tilemapGenerator : MonoBehaviour
     public int levelheight;
     public float[] maxLevelHeight;
     public float[] maxLevelWidth;
+    public int[] platformlength;
     public Tile main;
-    // Start is called before the first frame update
+
     void Start()
     {
         levelWidth =Mathf.CeilToInt(Random.Range(maxLevelWidth[0], maxLevelWidth[1]));
         generateplatform(levelheight,levelWidth);
-        
     }
+
     void generateplatform(int height, int width)
     {
+        int previousheight;
         for (int i=0; i< width; i++)
         {
             levelheight= Mathf.CeilToInt(Random.Range(maxLevelHeight[0], maxLevelHeight[1]));
+            
+            height = levelheight;
             Environment[0].SetTile(new Vector3Int(i, height, Mathf.CeilToInt(gameObject.transform.position.z)), main);
         }
     }
-    
 }
