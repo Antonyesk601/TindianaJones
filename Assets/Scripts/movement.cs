608198151bbffda282ef0b4d335c0e5ddc541a91
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2020-04-02 18:44:08 +0200

    Added Project Files

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class movement : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        float moving = Input.GetAxis("Horizontal") * speed;
+        if (moving > speed)
+        {
+            moving = speed;
+            Debug.Log(moving + " " + speed);
+        }
+        gameObject.transform.Translate(moving, 0, 0);
+        
+
+    }
+}
---
+++ Assets/Scripts/tilemapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class tilemapGenerator : MonoBehaviour
+{
+    public Grid grid;
+    public List<Tilemap> Environment= new List<Tilemap>();
+    public int levelWidth;
+    public int levelheight;
+    public Tile main;
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateplatform(levelheight,levelWidth);
+        
+    }
+    void generateplatform(int height, int width)
+    {
+        Environment[0].BoxFill(new Vector3Int(0, 0, 0), main, 0, 0, width, height); 
+    }
+    
+}
