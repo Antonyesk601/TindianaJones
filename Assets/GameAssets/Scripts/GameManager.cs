0032f91289832ce4359d31cc813e94d82db6b1ee
Kareem Morsy <kreem.morsy@hotmail.com>
2020-04-03 20:08:25 +0200

    Added Main Menu

---
+++ Assets/GameAssets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int lives = 3;
+    public int score = 0;
+    public int highScore = 0;
+
+    private int _level = 0;
+    public int level
+    {
+        get => _level; set
+        {
+            SceneManager.LoadSceneAsync(value);
+            _level = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void Quite()
+    {
+        Application.Quit();
+    }
+
+}
