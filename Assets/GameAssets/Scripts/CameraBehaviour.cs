ac4fcfe912b456c472724238905f43cf76a6d10b
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2020-04-03 23:18:59 +0200

    glitchcam

--- Assets/GameAssets/Scripts/CameraBehaviour.cs
+++ Assets/GameAssets/Scripts/CameraBehaviour.cs
@@ -6,22 +6,29 @@ public class CameraBehaviour : MonoBehaviour
 {
     public Camera maincam;
     public Camera glitchcam;
-
-    void Update()
+    bool x;
+    private void Start()
     {
-        glitchcam.gameObject.transform.position = maincam.gameObject.transform.position;
-        glitchcam.gameObject.transform.rotation = maincam.gameObject.transform.rotation;
         StartCoroutine(screenglitch());
+        x = true;
     }
+
+
     IEnumerator screenglitch()
     {
+        while (x) {
+            glitchcam.gameObject.transform.position = maincam.gameObject.transform.position;
+            glitchcam.gameObject.transform.rotation = maincam.gameObject.transform.rotation;
+            yield return new WaitForSecondsRealtime(10);
+            maincam.gameObject.SetActive(false);
+            glitchcam.gameObject.SetActive(true);
+            yield return new WaitForSecondsRealtime(2);
+            maincam.gameObject.SetActive(true);
+            glitchcam.gameObject.SetActive(false);
 
-        yield return new WaitForSeconds(10);
-        maincam.gameObject.SetActive(false);
-        glitchcam.gameObject.SetActive(true);
-        yield return new WaitForSeconds(2);
-        maincam.gameObject.SetActive(true);
-        glitchcam.gameObject.SetActive(false);
+        } 
     }
-}
-}
+
+    }
+
+
