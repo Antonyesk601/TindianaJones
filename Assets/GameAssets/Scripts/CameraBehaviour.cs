432f0eb66e4cf00cb8a38c629b2c532d6433779f
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2020-04-03 22:07:44 +0200

    edits

--- Assets/GameAssets/Scripts/CameraBehaviour.cs
+++ Assets/GameAssets/Scripts/CameraBehaviour.cs
@@ -8,6 +8,15 @@ public class CameraBehaviour : MonoBehaviour
     public GameObject player;
     void Update()
     {
-                
+        StartCoroutine(screenglitch());
+        Debug.Log("coroutine finished");
+    }
+    IEnumerator screenglitch()
+    {
+        yield return new WaitForSeconds(15);
+        maincam.cullingMask = (1 << LayerMask.NameToLayer("Ground"));
+        Debug.Log(true);
+        yield return new WaitForSeconds(3);
+        maincam.cullingMask = (1 << LayerMask.NameToLayer("UI")& LayerMask.NameToLayer("Ground") << LayerMask.NameToLayer("false"));
     }
 }
