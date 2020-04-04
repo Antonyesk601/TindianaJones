e70db2f48d8dc5117c120c37e31647a7e6af5801
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2020-04-04 20:06:56 +0200

    level 2

---
+++ Assets/GameAssets/Scripts/chargedtile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chargedtile : MonoBehaviour
+{
+    public isCharged activate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activate.ischarged)
+        {
+            gameObject.SetActive(true);
+            
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
