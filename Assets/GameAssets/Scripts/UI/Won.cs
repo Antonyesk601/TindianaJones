5f7fc1e388ef19f1212c0771eab4e491e8fb09e1
Kareem Morsy <kreem.morsy@hotmail.com>
2020-04-04 16:34:57 +0200

    Added Won UI

---
+++ Assets/GameAssets/Scripts/UI/Won.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Won : MonoBehaviour
+{
+    public int nextLevelNumber;
+
+    private GameManager gm;
+
+    void Start()
+    {
+        gm = GameManager.Instance;
+    }
+
+    public void goToLevel()
+    {
+        gm.level = nextLevelNumber;
+    }
+
+    public void ResetLives()
+    {
+        gm.level = nextLevelNumber;
+        gm.score = 0;
+        gm.lives = 3;
+    }
+}
